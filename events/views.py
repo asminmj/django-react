a9073b3da83b08efab9dd0b104f2415ba9e5a7d7
asminmj <asminmj13@gmail.com>
2024-01-30 19:07:13 +0545

    drf

---
+++ events/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Events
+from .serializers import LeadSerializer
+from rest_framework import generics
+
+# Create your views here.
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Events.objects.all()
+    serializer_class = LeadSerializer
